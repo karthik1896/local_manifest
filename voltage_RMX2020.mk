956d9267ad6b0c70d668f657b04663b8a5d36aaa
karthik1896 <karthik1896@outlook.com>
2023-03-28 17:04:31 +0530

    RMX_2020: Adapt to VoltageOS

--- voltage_RMX2020.mk
+++ voltage_RMX2020.mk
@@ -12,11 +12,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Inherit from device makefile
 $(call inherit-product, device/realme/RMX2020/device.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common VoltageOS stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_RMX2020
+PRODUCT_NAME := voltage_RMX2020
 PRODUCT_DEVICE := RMX2020
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX2020
@@ -26,9 +26,7 @@ PRODUCT_SYSTEM_MODEL := RMX2020
 PRODUCT_SYSTEM_NAME := RMX2020
 PRODUCT_SYSTEM_DEVICE := RMX2020
 
-# ArrowOS additions
-DEVICE_MAINTAINER := sarthakroy2002
-TARGET_BOOT_ANIMATION_RES := 720
+TARGET_BOOT_ANIMATION_RES := 1280
 
 # Build info
 BUILD_FINGERPRINT := "google/sunfish/sunfish:13/TQ1A.230205.002/9471150:user/release-keys"
